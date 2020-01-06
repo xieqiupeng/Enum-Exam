e1cab7386ccbece08468ab75b37de84b09041d75
xieqiupeng <xieqiupeng@gmail.com>
2020-01-06 08:41:49 +0800

    update

---
+++ dependency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/Classify/WWG-JYRK  $Path/万维钢-精英日课
+ln -sf /Volumes/Project/Classify/ZSJSLL  $Path/知识就是力量
+
+# Root
+Path=/Volumes/Project/Enum/Enum-Exam
+
+
+
+
+
+
+
+
+
+
